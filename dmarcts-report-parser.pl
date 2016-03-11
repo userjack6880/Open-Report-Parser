cdd790a62f2fac52a26eaea8ded8b38accb95cb2
TechSneeze <dave@techsneeze.com>
2016-03-10 16:04:40 -0800

    Update dmarcts-report-parser.pl

--- dmarcts-report-parser.pl
+++ dmarcts-report-parser.pl
@@ -7,7 +7,23 @@
 #  ...as well as John Levine's rddmarc:
 #  http://www.taugh.com/rddmarc  
 ###################################################################
-
+# dmarcts-report-parser - A Perl DMARC report parser
+# Copyright (C) 2016 TechSneeze.com
+#
+# https://github.com/techsneeze/dmarcts-report-parser
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+###################################################################
 
 # Usage:
 #    ./dmarcts-report-parser.pl [OPTIONS] [PATH]
